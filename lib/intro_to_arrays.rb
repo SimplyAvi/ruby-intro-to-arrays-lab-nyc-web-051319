954846f17e2a19378bd39dc18fd0b52db7005dc9
Avitosh Totaram <a.totaram@gmail.com>
2019-05-04 21:16:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,8 @@
+
+instantiate_new_array = Array.new
+
+array_with_two_elements = Array.new(obj1,obj2)
+array_with_two_elements[first_element]
+array_with_two_elements[second_element]
+array_with_two_elements[third_element]
+array_with_two_elements[last_element]
